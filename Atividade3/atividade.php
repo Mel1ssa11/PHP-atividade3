88e768e2129c7d3bc7a4d43a4ed953013efe46a9
𝐌𝐞𝐥𝐢𝐬𝐬𝐚 𝐌𝐚𝐫𝐭𝐢𝐧𝐬 <martinsdossantosmelissa093@gmail.com>
2026-03-05 17:57:44 -0300

    Add files via upload

---
+++ Atividade3/atividade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre mim</title>
+</head>
+
+<body>
+
+    <?php
+
+    $nome = "Melissa";
+    $sobrenome = " Martins dos Santos";
+
+    echo $nome . $sobrenome;
+
+    $idade = "
+    16 anos";
+    echo $idade;
+    
+    $nascimento = "
+    01/03/2010
+    ";
+    echo $nascimento;
+
+    ?>
+
+</body>
+
+</html>
